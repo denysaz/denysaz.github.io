31e9184b462296263489bf08a33b88bf9bfd3ea9
denysaz <den.zverev46@gmail.com>
2020-01-22 13:24:11 +0200

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    var myAlt = myImage.getAttribute('alt');
+    if (mySrc === 'images/youtube-icon.svg.png') {
+      myImage.setAttribute ('src','images/mem.jpg');
+      myImage.setAttribute ('alt','picture has changed');
+    } else {
+      myImage.setAttribute ('src','images/youtube-icon.svg.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+  }
