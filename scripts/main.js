a3e3a2112b1bd5e104fc7d7b91b0a6090b4d262e
denysaz <den.zverev46@gmail.com>
2020-01-22 13:31:05 +0200

    remove  what_is_your_name function

--- scripts/main.js
+++ scripts/main.js
@@ -11,22 +11,22 @@ myImage.onclick = function() {
     }
 }
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
 
-function setUserName() {
-    var myName = prompt('Please enter your name.');
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
-}
+// function setUserName() {
+//     var myName = prompt('Please enter your name.');
+//     localStorage.setItem('name', myName);
+//     myHeading.textContent = 'Mozilla is cool, ' + myName;
+// }
 
-if(!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    var storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Mozilla is cool, ' + storedName;
-}
+// if(!localStorage.getItem('name')) {
+//     setUserName();
+// } else {
+//     var storedName = localStorage.getItem('name');
+//     myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// }
 
-myButton.onclick = function() {
-    setUserName();
-  }
+// myButton.onclick = function() {
+//     setUserName();
+//   }
